d34b40608bb6a0f3f5192f8a377ef29d3540109f
Darren Lobo <darren3642@gmail.com>
2019-04-23 04:36:09 +0100

    Commit before clearing database

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module FilmReview
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Force all access to the app over SSL, use Strict-Transport-Security,
+    # and use secure cookies.
+    config.force_ssl = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
